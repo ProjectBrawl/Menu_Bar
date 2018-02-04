577b196f9554344140dd21ff106d01d7f3c7b875
zifanshi <zifanshi@usc.edu>
2018-02-04 14:48:40 -0800

    Now it's all microservices, I hope the fad persists.

---
+++ Menu bar/Assets/Script/MouseHoover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHoover : MonoBehaviour {
+	public Renderer rend;
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+		rend.material.color = Color.black;
+	}
+	void OnMouseEnter() {
+		rend.material.color = Color.red;
+	}
+	void OnMouseExit() {
+		rend.material.color = Color.black;
+	}
+		
+}
